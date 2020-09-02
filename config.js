2d2018ea110cb04208dcf4d827a3188040d8cc72
Patrick Obermeier <patrick.obermeier@outlook.com>
2020-09-02 17:46:32 +0200

    Updated default to-time

--- config.js
+++ config.js
@@ -1,9 +1,11 @@
 module.exports = {
   UTC_KEEP_ALIVE_FROM: 7,
-  UTC_KEEP_ALIVE_TO: 20,
+  UTC_KEEP_ALIVE_TO: 23,
   URIS_TO_KEEP_ALIVE: [
     'https://battleship-websockets-game.herokuapp.com/',
     'https://devmeetup-chat.herokuapp.com/',
     'https://mern-stack-app-social-network.herokuapp.com/',
+    'https://sdfsdfsdfsdf.com/',
   ],
+  PING_INTERVAL: 900000, // 15 Minutes
 };
