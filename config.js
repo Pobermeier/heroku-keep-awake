c012cac8c62db3fe9e4e3a257db5ab3f2ea11f9c
Patrick Obermeier <patrick.obermeier@outlook.com>
2020-09-02 17:47:41 +0200

    Removed default URIs

--- config.js
+++ config.js
@@ -1,11 +1,6 @@
 module.exports = {
   UTC_KEEP_ALIVE_FROM: 7,
   UTC_KEEP_ALIVE_TO: 23,
-  URIS_TO_KEEP_ALIVE: [
-    'https://battleship-websockets-game.herokuapp.com/',
-    'https://devmeetup-chat.herokuapp.com/',
-    'https://mern-stack-app-social-network.herokuapp.com/',
-    'https://sdfsdfsdfsdf.com/',
-  ],
+  URIS_TO_KEEP_ALIVE: [],
   PING_INTERVAL: 900000, // 15 Minutes
 };
