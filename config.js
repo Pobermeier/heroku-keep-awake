b9b365e7d4f039ce111422a2d5fe97615dd187c1
Patrick Obermeier <patrick.obermeier@outlook.com>
2020-09-02 16:54:01 +0200

    Added global config file

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  UTC_KEEP_ALIVE_FROM: 7,
+  UTC_KEEP_ALIVE_TO: 20,
+  URIS_TO_KEEP_ALIVE: [
+    'https://battleship-websockets-game.herokuapp.com/',
+    'https://devmeetup-chat.herokuapp.com/',
+    'https://mern-stack-app-social-network.herokuapp.com/',
+  ],
+};
